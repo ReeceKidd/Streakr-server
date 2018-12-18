2a799af9c2da788448a3cc8baf0f5ed20910440e
Reece Kidd <reecekidd95@gmail.com>
2018-12-18 20:55:48 +0000

    feat: Added hashPassword middleware and unit tests

---
+++ src/Middleware/Password/hashPassword.ts
@@ -0,0 +1,17 @@
+import { Request, Response, NextFunction } from "express";
+
+const hashPassword = (hash, salt) => async (
+  request: Request,
+  response: Response,
+  next: NextFunction
+) => {
+  try {
+    const { password } = request.body;
+    response.locals.hashedPassword = await hash(password, salt);
+    next();
+  } catch(err){
+    next(err)
+  }
+};
+
+export { hashPassword };
--- src/Routes/routes.ts
+++ src/Routes/routes.ts
@@ -1,10 +1,18 @@
 import { UserValidation } from "../validation/user.validation";
 import { celebrate, errors } from "celebrate";
 import { UserUtils } from "../Middleware/user.utils";
-import { PasswordHelper } from "../Middleware/passsord.helper";
+import * as bcrypt from 'bcrypt'
+
 import  UserRouter from "../Logic/User/user";
 import UserModel from "../Models/User"
 import { doesUserEmailExist } from "../Middleware/Database/doesEmailExist";
+import { emailExistsValidation} from "../Middleware/Validation/emailExistsValidation";
+import { ErrorMessageHelper } from "../Utils/errorMessage.helper";
+import { doesUserNameExist } from "../Middleware/Database/doesUserNameExist";
+import { userNameExistsValidation } from "../Middleware/Validation/userNameExistsValidation";
+import { hashPassword } from "../Middleware/Password/hashPassword";
+
+const SALT_ROUNDS = 10
 
 export class Routes {
   public routes(app): void {
@@ -16,12 +24,11 @@ export class Routes {
       .route("/user/register")
       .post(
         celebrate(UserValidation.register),
-        //doesUserEmailExist(),
-        //UserValidationMiddleware.emailExistsValidation,
-        //UserDatabaseHelper.doesUserNameExist,
-        //UserValidationMiddleware.userNameExistsValidation,
-        PasswordHelper.injectDependencies,
-        PasswordHelper.setHashedPassword,
+        doesUserEmailExist(UserModel.findOne),
+        emailExistsValidation(ErrorMessageHelper.generateAlreadyExistsMessage),
+        doesUserNameExist(UserModel.findOne),
+        userNameExistsValidation(ErrorMessageHelper.generateAlreadyExistsMessage),
+        hashPassword(bcrypt.hash, SALT_ROUNDS),
         UserUtils.createUserFromRequest,
        // UserDatabaseHelper.saveUserToDatabase
       );
