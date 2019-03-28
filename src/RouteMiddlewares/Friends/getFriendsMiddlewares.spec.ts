3fec619bc03030afd7813af51a3e63677a18bc06
Reece Kidd <reecekidd95@gmail.com>
2019-03-28 07:14:59 +0000

    feat: Added unit tests for getUserExistsValidationMiddleware on getFriends

--- src/RouteMiddlewares/Friends/getFriendsMiddlewares.spec.ts
+++ src/RouteMiddlewares/Friends/getFriendsMiddlewares.spec.ts
@@ -1,4 +1,4 @@
-import { getFriendsMiddlewares, getFriendsValidationMiddleware, formatFriendsMiddleware, userExistsValidationMiddleware, retreiveFriendsMiddleware, sendFormattedFriendsMiddleware, getRetreiveUserMiddleware, retreiveUserMiddleware } from "./getFriendsMiddlewares";
+import { getFriendsMiddlewares, getFriendsValidationMiddleware, formatFriendsMiddleware, userExistsValidationMiddleware, getUserExistsValidationMiddleware, retreiveFriendsMiddleware, sendFormattedFriendsMiddleware, getRetreiveUserMiddleware, retreiveUserMiddleware } from "./getFriendsMiddlewares";
 
 describe(`getFriendsValidationMiddleware`, () => {
 
@@ -128,6 +128,70 @@ describe(`retreiveUserMiddleware`, () => {
         expect(findOne).toBeCalledWith({ _id: mockUserId });
         expect(response.locals.user).toBe(undefined);
         expect(next).toBeCalledWith(ERROR_MESSAGE);
+    });
+});
+
+describe(`userExistsValidationMiddleware`, () => {
+    const mockErrorMessage = 'User does not exist'
+
+    test("check that error response is returned correctly when user wasn't found", async () => {
+        const send = jest.fn();
+        const status = jest.fn(() => ({ send }));
+
+        const request: any = {};
+        const response: any = {
+            locals: {},
+            status
+        };
+        const next = jest.fn();
+
+        const middleware = getUserExistsValidationMiddleware(mockErrorMessage);
+
+        middleware(request, response, next);
+
+        expect.assertions(2);
+        expect(status).toHaveBeenCalledWith(400);
+        expect(send).toBeCalledWith({ message: mockErrorMessage });
+    });
+
+    test("check that next is called when user is defined on response.locals", () => {
+        const send = jest.fn();
+        const status = jest.fn(() => ({ send }));
+
+        const request: any = {};
+        const response: any = {
+            locals: { user: true },
+            status
+        };
+        const next = jest.fn();
+
+        const middleware = getUserExistsValidationMiddleware(mockErrorMessage);
+
+        middleware(request, response, next);
+
+        expect.assertions(3);
+        expect(status).not.toHaveBeenCalled();
+        expect(send).not.toBeCalled();
+        expect(next).toBeCalled();
+    });
+
+    test("check that next is called with err on send failure", () => {
+        const errorMessage = 'error'
+        const send = jest.fn(() => { throw new Error(errorMessage) });
+        const status = jest.fn(() => ({ send }));
+        const request: any = {};
+        const response: any = {
+            locals: { user: false },
+            status
+        };
+        const next = jest.fn();
+
+        const middleware = getUserExistsValidationMiddleware(mockErrorMessage);
+
+        middleware(request, response, next);
+
+        expect.assertions(1);
+        expect(next).toBeCalledWith(new Error(errorMessage));
     });
 });
 
