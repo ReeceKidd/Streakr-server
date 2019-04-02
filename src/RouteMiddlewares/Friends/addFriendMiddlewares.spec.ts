6b62cc994b620cd414f87b35510c25dda9eb8554
Reece Kidd <reecekidd95@gmail.com>
2019-04-02 05:40:33 +0100

    feat: Added unit tests to the addFriend middleware

--- src/RouteMiddlewares/Friends/addFriendMiddlewares.spec.ts
+++ src/RouteMiddlewares/Friends/addFriendMiddlewares.spec.ts
@@ -1,5 +1,127 @@
 import { addFriendMiddlewares, addFriendValidationMiddleware, retreiveUserMiddleware, userExistsValidationMiddleware, addFriendMiddleware, sendSuccessMessageMiddleware } from "./addFriendMiddlewares";
 
+describe('addFriendValidationMiddleware', () => {
+
+    const mockUserId = '1234'
+    const mockFriendId = '2345'
+
+    test('that valid request passes', () => {
+
+        const send = jest.fn();
+        const status = jest.fn(() => ({ send }));
+
+        const request: any = {
+            body: { userId: mockUserId, friendId: mockFriendId }
+        };
+        const response: any = {
+            status
+        };
+        const next = jest.fn();
+
+        addFriendValidationMiddleware(request, response, next);
+
+        expect.assertions(1);
+        expect(next).toBeCalledWith();
+
+    })
+
+    test('that request fails when userId is missing from body', () => {
+
+        const send = jest.fn();
+        const status = jest.fn(() => ({ send }));
+
+        const request: any = {
+            body: { friendId: mockFriendId }
+        };
+        const response: any = {
+            status
+        };
+        const next = jest.fn();
+
+        addFriendValidationMiddleware(request, response, next);
+
+        expect.assertions(3)
+        expect(status).toHaveBeenCalledWith(422);
+        expect(send).toBeCalledWith({
+            message: 'child "userId" fails because ["userId" is required]'
+        });
+        expect(next).not.toBeCalled();
+
+    })
+
+    test('that request fails when userId is not a string', () => {
+
+        const send = jest.fn();
+        const status = jest.fn(() => ({ send }));
+
+        const request: any = {
+            body: { userId: 1234, friendId: mockFriendId }
+        };
+        const response: any = {
+            status
+        };
+        const next = jest.fn();
+
+        addFriendValidationMiddleware(request, response, next);
+
+        expect.assertions(3)
+        expect(status).toHaveBeenCalledWith(422);
+        expect(send).toBeCalledWith({
+            message: 'child "userId" fails because ["userId" must be a string]'
+        });
+        expect(next).not.toBeCalled();
+
+    })
+
+    test('that request fails when friendId is missing from body', () => {
+
+        const send = jest.fn();
+        const status = jest.fn(() => ({ send }));
+
+        const request: any = {
+            body: { userId: mockUserId }
+        };
+        const response: any = {
+            status
+        };
+        const next = jest.fn();
+
+        addFriendValidationMiddleware(request, response, next);
+
+        expect.assertions(3)
+        expect(status).toHaveBeenCalledWith(422);
+        expect(send).toBeCalledWith({
+            message: 'child "friendId" fails because ["friendId" is required]'
+        });
+        expect(next).not.toBeCalled();
+
+    })
+
+    test('that request fails when friendId is not a string', () => {
+
+        const send = jest.fn();
+        const status = jest.fn(() => ({ send }));
+
+        const request: any = {
+            body: { userId: mockUserId, friendId: 1234 }
+        };
+        const response: any = {
+            status
+        };
+        const next = jest.fn();
+
+        addFriendValidationMiddleware(request, response, next);
+
+        expect.assertions(3)
+        expect(status).toHaveBeenCalledWith(422);
+        expect(send).toBeCalledWith({
+            message: 'child "friendId" fails because ["friendId" must be a string]'
+        });
+        expect(next).not.toBeCalled();
+
+    })
+})
+
 describe('addFriendMiddlewares', () => {
     test('that middlewares are defined in the correct order', () => {
         expect.assertions(5)
--- src/RouteMiddlewares/Friends/addFriendMiddlewares.ts
+++ src/RouteMiddlewares/Friends/addFriendMiddlewares.ts
@@ -1,7 +1,15 @@
 import { Request, Response, NextFunction } from "express";
+import * as Joi from 'joi'
+
+import { getValidationErrorMessageSenderMiddleware } from "../../SharedMiddleware/validationErrorMessageSenderMiddleware";
+
+const addFriendValidationSchema = {
+    userId: Joi.string().required(),
+    friendId: Joi.string().required()
+}
 
 export const addFriendValidationMiddleware = (request: Request, response: Response, next: NextFunction) => {
-
+    Joi.validate(request.body, addFriendValidationSchema, getValidationErrorMessageSenderMiddleware(request, response, next))
 }
 
 export const retreiveUserMiddleware = (request: Request, response: Response, next: NextFunction) => {
