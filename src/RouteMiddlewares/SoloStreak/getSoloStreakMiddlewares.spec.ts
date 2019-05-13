ee78bfcd0e11cdf2e7dd9989e9d40b04df3c1c79
Reece Kidd <reecekidd95@gmail.com>
2019-05-13 05:31:35 +0100

    feat: Added unit tests for getSoloStreakMiddlewares

--- src/RouteMiddlewares/SoloStreak/getSoloStreakMiddlewares.spec.ts
+++ src/RouteMiddlewares/SoloStreak/getSoloStreakMiddlewares.spec.ts
@@ -1,6 +1,75 @@
-import { getSoloStreakMiddlewares, retreiveSoloStreakMiddleware, getRetreiveSoloStreakMiddleware, sendSoloStreakMiddleware, getSoloStreakParamsValidationMiddleware } from "./getSoloStreakMiddlewares";
+import { getSoloStreakMiddlewares, retreiveSoloStreakMiddleware, getRetreiveSoloStreakMiddleware, sendSoloStreakMiddleware, getSoloStreakParamsValidationMiddleware, getSendSoloStreakMiddleware } from "./getSoloStreakMiddlewares";
+import { ResponseCodes } from "../../Server/responseCodes";
 
-describe('findSoloStreakMiddleware', () => {
+describe(`getSoloStreakParamsValidationMiddleware`, () => {
+
+    const soloStreakId = '12345678'
+
+    test("that next() is called when correct params are supplied", () => {
+        expect.assertions(1);
+        const send = jest.fn();
+        const status = jest.fn(() => ({ send }));
+
+        const request: any = {
+            params: { soloStreakId }
+        };
+        const response: any = {
+            status
+        };
+        const next = jest.fn();
+
+        getSoloStreakParamsValidationMiddleware(request, response, next);
+        expect(next).toBeCalled();
+    });
+
+
+    test("that correct response is sent when soloStreakId is missing", () => {
+        expect.assertions(3);
+        const send = jest.fn();
+        const status = jest.fn(() => ({ send }));
+
+        const request: any = {
+            params: {}
+        };
+        const response: any = {
+            status
+        };
+        const next = jest.fn();
+
+        getSoloStreakParamsValidationMiddleware(request, response, next);
+
+        expect(status).toHaveBeenCalledWith(ResponseCodes.unprocessableEntity);
+        expect(send).toBeCalledWith({
+            message: 'child "soloStreakId" fails because ["soloStreakId" is required]'
+        });
+        expect(next).not.toBeCalled();
+    });
+
+    test("that error response is sent when soloStreakId is not a string", () => {
+        expect.assertions(3);
+        const send = jest.fn();
+        const status = jest.fn(() => ({ send }));
+
+        const request: any = {
+            params: { soloStreakId: 1234 }
+        };
+        const response: any = {
+            status
+        };
+        const next = jest.fn();
+
+        getSoloStreakParamsValidationMiddleware(request, response, next);
+
+        expect(status).toHaveBeenCalledWith(ResponseCodes.unprocessableEntity);
+        expect(send).toBeCalledWith({
+            message: 'child "soloStreakId" fails because ["soloStreakId" must be a string]'
+        });
+        expect(next).not.toBeCalled();
+    });
+
+});
+
+describe('retreiveSoloStreakMiddleware', () => {
     test('that response.locals.soloStreak is defined and next() is called', async () => {
         expect.assertions(3)
         const findOne = jest.fn(() => Promise.resolve(true))
@@ -32,6 +101,41 @@ describe('findSoloStreakMiddleware', () => {
         const middleware = getRetreiveSoloStreakMiddleware(soloStreakModel)
         await middleware(request, response, next)
         expect(next).toBeCalledWith(errorMessage)
+    })
+})
+
+describe('sendSoloStreakMiddleware', () => {
+    test('that soloStreak is sent correctly', () => {
+        expect.assertions(3);
+        const send = jest.fn()
+        const status = jest.fn(() => ({ send }))
+        const soloStreak = { _id: 'abc' }
+        const request: any = {}
+        const response: any = { locals: { soloStreak }, status };
+        const next = jest.fn();
+
+        const resourceCreatedCode = 401
+        const middleware = getSendSoloStreakMiddleware(resourceCreatedCode)
+        middleware(request, response, next);
+
+        expect(next).not.toBeCalled()
+        expect(status).toBeCalledWith(resourceCreatedCode)
+        expect(send).toBeCalledWith({ ...soloStreak })
+
+    })
+
+    test('that on error next is called with error', async () => {
+        expect.assertions(1)
+        const request: any = {}
+        const error = 'error'
+        const send = jest.fn(() => Promise.reject(error))
+        const status = jest.fn(() => ({ send }))
+        const response: any = { status }
+        const next = jest.fn()
+        const resourceCreatedResponseCode = 401
+        const middleware = getSendSoloStreakMiddleware(resourceCreatedResponseCode)
+        await middleware(request, response, next)
+        expect(next).toBeCalledWith(new TypeError("Cannot destructure property `soloStreak` of 'undefined' or 'null'."))
     })
 })
 
--- src/RouteMiddlewares/SoloStreak/getSoloStreakMiddlewares.ts
+++ src/RouteMiddlewares/SoloStreak/getSoloStreakMiddlewares.ts
@@ -2,6 +2,7 @@ import { Request, Response, NextFunction } from 'express'
 import * as Joi from 'joi'
 import { getValidationErrorMessageSenderMiddleware } from '../../SharedMiddleware/validationErrorMessageSenderMiddleware';
 import { soloStreakModel } from '../../Models/SoloStreak';
+import { ResponseCodes } from '../../Server/responseCodes';
 
 
 const getSoloStreakParamsValidationSchema = {
@@ -25,9 +26,16 @@ export const getRetreiveSoloStreakMiddleware = soloStreakModel => async (request
 
 export const retreiveSoloStreakMiddleware = getRetreiveSoloStreakMiddleware(soloStreakModel)
 
-export const sendSoloStreakMiddleware = (request: Request, response: Response, next: NextFunction) => {
-
+export const getSendSoloStreakMiddleware = (resourceCreatedResponseCode: number) => (request: Request, response: Response, next: NextFunction) => {
+    try {
+        const { soloStreak } = response.locals
+        return response.status(resourceCreatedResponseCode).send({ ...soloStreak })
+    } catch (err) {
+        next(err)
+    }
 }
+
+export const sendSoloStreakMiddleware = getSendSoloStreakMiddleware(ResponseCodes.created)
 
 export const getSoloStreakMiddlewares = [
     getSoloStreakParamsValidationMiddleware,
