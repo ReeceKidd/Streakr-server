c442dcc20ad4fc4513d697cb9cfacd4226b5e457
Reece Kidd <reecekidd95@gmail.com>
2019-05-15 05:58:06 +0100

    feat: Added getPatchSoloStreakMiddleware and unit tests

--- src/RouteMiddlewares/SoloStreak/patchSoloStreakMiddlewares.spec.ts
+++ src/RouteMiddlewares/SoloStreak/patchSoloStreakMiddlewares.spec.ts
@@ -115,34 +115,36 @@ describe('soloStreakRequestBodyValidationMiddleware', () => {
 
 })
 
-// describe('patchSoloStreakMiddleware', () => {
-//     test('that response.locals.updatedSoloStreak is defined and next is called', async () => {
-//         expect.assertions(3)
-//         const soloStreakId = 'abc123'
-//         const userId = '123cde'
-//         const name = 'Daily programming'
-//         const description = 'Do one hour of programming each day'
-//         const request: any = {
-//             paramas: { soloStreakId },
-//             body: {
-//                 userId,
-//                 name,
-//                 description
-//             }
-//         }
-//         const response: any = { locals: {} }
-//         const next = jest.fn()
-//         const lean = jest.fn(() => true)
-//         const updateOne = jest.fn(() => (Promise.resolve({ lean })))
-//         const soloStreakModel = { updateOne }
-//         const middleware = getPatchSoloStreakMiddleware(soloStreakModel)
-//         await middleware(request, response, next)
-//         expect(updateOne).toBeCalledWith({ _id: soloStreakId }, { userId, name, description })
-//         expect(lean).toBeCalledWith()
-//         expect(response.locals.updatedSoloStreak).toBeDefined()
-//         expect(next).toBeCalledWith()
-//     })
-// })
+describe('patchSoloStreakMiddleware', () => {
+    test('that response.locals.updatedSoloStreak is defined and next is called', async () => {
+        expect.assertions(4)
+        const soloStreakId = 'abc123'
+        const userId = '123cde'
+        const name = 'Daily programming'
+        const description = 'Do one hour of programming each day'
+        const request: any = {
+            params: { soloStreakId },
+            body: {
+                userId,
+                name,
+                description
+            }
+        }
+        const response: any = { locals: {} }
+        const next = jest.fn()
+        const lean = jest.fn(() => (Promise.resolve(true)))
+        const updateOne = jest.fn(() => (({ lean })))
+        const soloStreakModel = {
+            updateOne
+        }
+        const middleware = getPatchSoloStreakMiddleware(soloStreakModel)
+        await middleware(request, response, next)
+        expect(updateOne).toBeCalledWith({ _id: soloStreakId }, { userId, name, description })
+        expect(lean).toBeCalledWith()
+        expect(response.locals.updatedSoloStreak).toBeDefined()
+        expect(next).toBeCalledWith()
+    })
+})
 
 describe('patchSoloStreakMiddlewares', () => {
 
