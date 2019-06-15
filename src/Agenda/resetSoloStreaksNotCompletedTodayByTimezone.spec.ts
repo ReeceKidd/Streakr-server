60034bfe27340fe7b2224e3de7c4a322679cad2a
Reece Kidd <reecekidd95@gmail.com>
2019-06-15 20:56:50 +0100

    refactor: Updated timezone for agenda intergration tests

---
+++ src/Agenda/resetSoloStreaksNotCompletedTodayByTimezone.spec.ts
@@ -0,0 +1,19 @@
+import { resetSoloStreaksNotCompletedTodayByTimezone } from "./resetSoloStreaksNotCompletedTodayByTimezone";
+
+describe("resetSoloStreaksNotCompletedTodayByTimezone", () => {
+    test("that function calls functions it requires correctly", async () => {
+        const soloStreakModel = {} as any;
+        const incompleteSoloStreaks = [];
+        const getIncompleteSoloStreaks = jest.fn(() => Promise.resolve(incompleteSoloStreaks));
+        const resetIncompleteSoloStreaks = jest.fn();
+        const timezone = "Europe/London";
+        const defaultCurrentStreak = {
+            startDate: undefined,
+            numberOfDaysInARow: 0
+        };
+        const endDate = new Date();
+        await resetSoloStreaksNotCompletedTodayByTimezone(soloStreakModel, getIncompleteSoloStreaks, resetIncompleteSoloStreaks, timezone, defaultCurrentStreak, endDate);
+        expect(getIncompleteSoloStreaks).toBeCalledWith(soloStreakModel, timezone);
+        expect(resetIncompleteSoloStreaks).toBeCalledWith(soloStreakModel, incompleteSoloStreaks, defaultCurrentStreak, endDate);
+    });
+});
