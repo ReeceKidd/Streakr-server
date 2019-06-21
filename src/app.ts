d96e051eca93a490a3f30f3aca073925c7a7e038
Reece Kidd <reecekidd95@gmail.com>
2019-06-21 19:06:51 +0100

    feat: Added cors

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,12 @@ import express from "express";
 import * as bodyParser from "body-parser";
 import mongoose from "mongoose";
 import passport from "passport";
+import dotenv from "dotenv";
+import cors from "cors";
+
 import ApiVersions from "./Server/versions";
 import v1Router from "./versions/v1";
-import dotenv from "dotenv";
+
 import { getServiceConfig } from "./getServiceConfig";
 import { errorHandler } from "./errorHandler";
 dotenv.config();
@@ -12,7 +15,9 @@ dotenv.config();
 const { DATABASE_URI } = getServiceConfig();
 
 const app = express();
+app.use(cors());
 app.use(bodyParser.json());
+
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get(`/health`, (request, response, next) => {
