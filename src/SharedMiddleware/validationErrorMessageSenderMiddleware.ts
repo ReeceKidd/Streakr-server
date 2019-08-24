5fded2ad123398b14f58dbd141f4eca1ad4b3124
Reece Kidd <reecekidd95@gmail.com>
2019-08-24 07:55:31 +0100

    chore: Removed unecessary console.logs

--- src/SharedMiddleware/validationErrorMessageSenderMiddleware.ts
+++ src/SharedMiddleware/validationErrorMessageSenderMiddleware.ts
@@ -10,18 +10,15 @@ export const getValidationErrorMessageSenderMiddleware = (
 ) => (error: Error): void => {
   if (error) {
     if (notAllowedParameterErrorRegExp.test(error.message)) {
-      console.log("a");
       response
         .status(ResponseCodes.badRequest)
         .send({ message: error.message });
     } else {
-      console.log("b");
       response
         .status(ResponseCodes.unprocessableEntity)
         .send({ message: error.message });
     }
   } else {
-    console.log("c");
     next();
   }
 };
