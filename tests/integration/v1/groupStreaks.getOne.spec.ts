3c3eceabed7433c0f0696309d420eeb7581f6750
Reece Kidd <reecekidd95@gmail.com>
2019-08-31 12:00:37 +0100

    feat: Added group one group streak intergration test

---
+++ tests/integration/v1/groupStreaks.getOne.spec.ts
@@ -0,0 +1,79 @@
+import streakoid from "../../../src/sdk/streakoid";
+
+const email = "get--one-group-streak@gmail.com";
+const username = "get-one-group-streak-user";
+
+const streakName = "Daily Meditation";
+const streakDescription = "Each day I must meditate at ";
+
+const timezone = "Europe/London";
+
+jest.setTimeout(120000);
+
+describe("GET /group-streaks/:groupStreakId", () => {
+  let userId: string;
+
+  let groupStreakId: string;
+
+  beforeAll(async () => {
+    const registrationResponse = await streakoid.users.create(username, email);
+    userId = registrationResponse.data._id;
+
+    const members = [userId];
+
+    const createGroupStreakResponse = await streakoid.groupStreaks.create(
+      userId,
+      streakName,
+      streakDescription,
+      members,
+      timezone
+    );
+    groupStreakId = createGroupStreakResponse.data._id;
+  });
+
+  afterAll(async () => {
+    await streakoid.users.deleteOne(userId);
+
+    await streakoid.groupStreaks.deleteOne(groupStreakId);
+  });
+
+  test(`group streak can be retreived with populated member information`, async () => {
+    expect.assertions(10);
+
+    const response = await streakoid.groupStreaks.getOne(groupStreakId);
+    const groupStreak = response.data;
+
+    expect(response.status).toEqual(200);
+    expect(groupStreak.members.length).toEqual(1);
+    expect(groupStreak.streakName).toEqual(streakName);
+    expect(groupStreak.streakDescription).toEqual(streakDescription);
+    expect(groupStreak.creatorId).toEqual(userId);
+    expect(groupStreak.timezone).toEqual(timezone);
+    expect(Object.keys(groupStreak)).toEqual([
+      "_id",
+      "members",
+      "creatorId",
+      "streakName",
+      "streakDescription",
+      "timezone",
+      "createdAt",
+      "updatedAt",
+      "__v"
+    ]);
+
+    const groupStreakMember = groupStreak.members[0];
+    expect(groupStreakMember.username).toEqual(username);
+    expect(groupStreakMember.email).toEqual(email);
+    expect(Object.keys(groupStreakMember)).toEqual([
+      "_id",
+      "type",
+      "streaks",
+      "friends",
+      "username",
+      "email",
+      "createdAt",
+      "updatedAt",
+      "__v"
+    ]);
+  });
+});
