68142d46cca78dc3f387f8e53838f54fc33876e8
Reece Kidd <reecekidd95@gmail.com>
2019-10-12 11:17:44 +0100

    feat: Updated dailyJobSchema

--- src/Models/DailyJob.ts
+++ src/Models/DailyJob.ts
@@ -7,6 +7,10 @@ export type DailyJobModel = DailyJob & mongoose.Document;
 
 export const dailyJobSchema = new mongoose.Schema(
     {
+        agendaJobId: {
+            type: String,
+            required: true,
+        },
         jobName: {
             type: AgendaJobNames,
             required: true,
