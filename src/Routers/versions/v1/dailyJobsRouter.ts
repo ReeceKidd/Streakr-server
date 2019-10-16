a72493579588c0fbe0a07a397a13324c7936235c
Reece Kidd <reecekidd95@gmail.com>
2019-10-16 15:48:30 +0100

    feat: Defined getAll route on dailyJobsRouter

--- src/Routers/versions/v1/dailyJobsRouter.ts
+++ src/Routers/versions/v1/dailyJobsRouter.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express';
 import { createDailyJobMiddlewares } from '../../../RouteMiddlewares/DailyJob/createDailyJobMiddlewares';
+import { getAllDailyJobsMiddlewares } from '../../../RouteMiddlewares/DailyJob/getAllDailyJobsMiddlewares';
 
 export const dailyJobId = 'dailyJobId';
 
 const dailyJobsRouter = Router();
 
 dailyJobsRouter.post(`/`, ...createDailyJobMiddlewares);
+
+dailyJobsRouter.get(`/`, ...getAllDailyJobsMiddlewares);
 
 export { dailyJobsRouter };
