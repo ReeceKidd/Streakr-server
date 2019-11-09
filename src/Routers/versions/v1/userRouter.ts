9695930247bdfb2c0743072566ad7c808f06d92e
Reece Kidd <reecekidd95@gmail.com>
2019-11-09 09:03:53 +0000

    feat: Added patch route to usersRouter

--- src/Routers/versions/v1/userRouter.ts
+++ src/Routers/versions/v1/userRouter.ts
@@ -1,8 +1,11 @@
 import { Router } from 'express';
 import { getCurrentUserMiddlewares } from '../../../../src/RouteMiddlewares/User/getCurrentUser';
+import { patchCurrentUserMiddlewares } from '../../../../src/RouteMiddlewares/User/patchCurrentUserMiddlewares';
 
 const userRouter = Router();
 
 userRouter.get('/', ...getCurrentUserMiddlewares);
+
+userRouter.patch('/', ...patchCurrentUserMiddlewares);
 
 export { userRouter };
