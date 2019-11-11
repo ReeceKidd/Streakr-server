b5e4b0192d247b05f10afb6533234ec9ea433218
Reece Kidd <reecekidd95@gmail.com>
2019-11-11 14:15:17 +0000

    feat: Got rid of unified topology

--- src/app.ts
+++ src/app.ts
@@ -27,7 +27,7 @@ app.get(`/health`, (request, response) => {
 mongoose.set('useNewUrlParser', true);
 mongoose.set('useFindAndModify', false);
 mongoose.set('useCreateIndex', true);
-mongoose.set('useUnifiedTopology', true);
+//mongoose.set('useUnifiedTopology', true);
 mongoose.connect(DATABASE_URI).catch(err => console.log(err.message));
 
 agenda
