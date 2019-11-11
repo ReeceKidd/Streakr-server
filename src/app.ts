99b8b7d2fdbec966992975815d20a229a24272fa
Reece Kidd <reecekidd95@gmail.com>
2019-11-11 14:17:37 +0000

    feat: Got rid of catch on mongo error

--- src/app.ts
+++ src/app.ts
@@ -28,7 +28,7 @@ mongoose.set('useNewUrlParser', true);
 mongoose.set('useFindAndModify', false);
 mongoose.set('useCreateIndex', true);
 //mongoose.set('useUnifiedTopology', true);
-mongoose.connect(DATABASE_URI).catch(err => console.log(err.message));
+mongoose.connect(DATABASE_URI);
 
 agenda
     .start()
