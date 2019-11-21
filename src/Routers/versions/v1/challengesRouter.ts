84f260badd8cadb53957c3f42dfb003a597f5f4c
Reece Kidd <reecekidd95@gmail.com>
2019-11-21 15:25:59 +0000

    refactor: Changed to getOneChallengeMiddlewares

--- src/Routers/versions/v1/challengesRouter.ts
+++ src/Routers/versions/v1/challengesRouter.ts
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 import { getAllChallengesMiddlewares } from '../../../RouteMiddlewares/Challenges/getAllChallengesMiddlewares';
 import { createChallengeMiddlewares } from '../../../RouteMiddlewares/Challenges/createChallengeMiddlewares';
-import { getChallengeMiddlewares } from '../../../RouteMiddlewares/Challenges/getOneChallengeMiddlewares';
+import { getOneChallengeMiddlewares } from '../../../RouteMiddlewares/Challenges/getOneChallengeMiddlewares';
 
 export const challengeId = 'challengeId';
 
@@ -9,7 +9,7 @@ const challengesRouter = Router();
 
 challengesRouter.get(`/`, ...getAllChallengesMiddlewares);
 
-challengesRouter.get(`/:${challengeId}`, ...getChallengeMiddlewares);
+challengesRouter.get(`/:${challengeId}`, ...getOneChallengeMiddlewares);
 
 challengesRouter.post('/', ...createChallengeMiddlewares);
 
