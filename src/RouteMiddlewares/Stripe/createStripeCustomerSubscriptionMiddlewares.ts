93c3e143f8d437b71dc501144ab21e3673333d1d
Reece Kidd <reecekidd95@gmail.com>
2019-12-23 10:31:41 +0000

    feat: Removed badges from formatted user

--- src/RouteMiddlewares/Stripe/createStripeCustomerSubscriptionMiddlewares.ts
+++ src/RouteMiddlewares/Stripe/createStripeCustomerSubscriptionMiddlewares.ts
@@ -228,7 +228,6 @@ export const formatUserMiddleware = (request: Request, response: Response, next:
             userType: user.userType,
             timezone: user.timezone,
             friends: user.friends,
-            badges: user.badges,
             createdAt: user.createdAt,
             updatedAt: user.updatedAt,
             profileImages: user.profileImages,
