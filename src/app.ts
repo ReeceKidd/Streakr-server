b5884052b999c17bb9469f2369d6707ee72eb7e4
Reece Kidd <reecekidd95@gmail.com>
2020-03-11 20:04:24 -0300

    feat: Removed error catcher for mongoose

--- src/app.ts
+++ src/app.ts
@@ -27,11 +27,8 @@ app.get(`/health`, (request, response) => {
 mongoose.set('useNewUrlParser', true);
 mongoose.set('useFindAndModify', false);
 mongoose.set('useCreateIndex', true);
-//mongoose.set('useUnifiedTopology', true);
 
-mongoose.connect(DATABASE_URI).catch(err => {
-    console.log('mongo err', err);
-});
+mongoose.connect(DATABASE_URI);
 
 agenda
     .start()
