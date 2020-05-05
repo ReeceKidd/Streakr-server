6539bab1317403e2376f48535bbd263611a82def
Reece Kidd <reecekidd95@gmail.com>
2020-05-05 10:25:46 +0100

    feat: Updated errorHandler unit tests to cover Sentry logic

--- src/errorHandler.spec.ts
+++ src/errorHandler.spec.ts
@@ -1,7 +1,66 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { errorHandler } from './errorHandler';
+jest.mock('@sentry/node', () => ({
+    setUser: jest.fn(),
+    captureException: jest.fn(),
+}));
+jest.mock('./getServiceConfig', () => ({
+    getServiceConfig: jest.fn(() => ({
+        NODE_ENV: 'production',
+    })),
+}));
+
+import * as Sentry from '@sentry/node';
 
 describe('errorHandler', () => {
+    test('if node env does not equal test and response.locals.user is undefined Sentry captures the exception without user data', () => {
+        expect.assertions(1);
+        const CustomError = {
+            httpStatusCode: 400,
+            body: {
+                code: '400',
+                message: 'Mock message',
+            },
+            localisedErrorMessage: 'Mock message',
+        } as any;
+        const request: any = {};
+        const send = jest.fn();
+        const status = jest.fn(() => ({ send }));
+        const response: any = {
+            locals: {},
+            status,
+        };
+        const next = jest.fn();
+
+        errorHandler(CustomError, request, response, next);
+
+        expect(Sentry.captureException).toBeCalledWith(CustomError);
+    });
+    test('if node env does not equal test and response.locals.user is defined Sentry captures the exception with user data', () => {
+        expect.assertions(2);
+        const CustomError = {
+            httpStatusCode: 400,
+            body: {
+                code: '400',
+                message: 'Mock message',
+            },
+        } as any;
+        const request: any = {};
+        const send = jest.fn();
+        const status = jest.fn(() => ({ send }));
+        const email = 'user@gmail.com';
+        const username = 'username';
+        const _id = 'id';
+        const response: any = {
+            locals: { user: { email, username, _id } },
+            status,
+        };
+        const next = jest.fn();
+
+        errorHandler(CustomError, request, response, next);
+        expect(Sentry.setUser).toBeCalledWith({ email, username, id: _id });
+        expect(Sentry.captureException).toBeCalledWith(CustomError);
+    });
     test('sends errors with httpStatusCode', () => {
         expect.assertions(2);
         const CustomError = {
@@ -16,6 +75,7 @@ describe('errorHandler', () => {
         const send = jest.fn();
         const status = jest.fn(() => ({ send }));
         const response: any = {
+            locals: {},
             status,
         };
         const next = jest.fn();
@@ -39,6 +99,7 @@ describe('errorHandler', () => {
         const send = jest.fn();
         const status = jest.fn(() => ({ send }));
         const response: any = {
+            locals: {},
             status,
         };
         const next = jest.fn();
