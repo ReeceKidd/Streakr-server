b22e631c5fb4c5c78339a2aef329f75eaf6f53cb
Reece Kidd <reecekidd95@gmail.com>
2020-06-01 11:43:49 +0100

    feat: Default UserType is temporary

--- src/Models/User.ts
+++ src/Models/User.ts
@@ -53,7 +53,7 @@ export const userSchema = new mongoose.Schema(
         userType: {
             type: String,
             enum: [UserTypes.basic, UserTypes.admin, UserTypes.temporary],
-            default: UserTypes.basic,
+            default: UserTypes.temporary,
         },
         timezone: {
             type: String,
