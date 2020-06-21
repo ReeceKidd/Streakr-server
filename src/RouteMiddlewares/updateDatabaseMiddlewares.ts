0fbb392069360eeb90a779c62d1d60458525b31f
Reece Kidd <reecekidd95@gmail.com>
2020-06-21 12:11:45 +0100

    chore: Changed updateDatabaseMiddlewares

--- src/RouteMiddlewares/updateDatabaseMiddlewares.ts
+++ src/RouteMiddlewares/updateDatabaseMiddlewares.ts
@@ -2,6 +2,10 @@
 
 // import { CustomError, ErrorType } from '../customError';
 // import { userModel } from '../../src/Models/User';
+// import { soloStreakModel } from '../Models/SoloStreak';
+// import { challengeStreakModel } from '../Models/ChallengeStreak';
+// import StreakStatus from '@streakoid/streakoid-models/lib/Types/StreakStatus';
+// import { teamMemberStreakModel } from '../Models/TeamMemberStreak';
 
 // export const updateDatabaseMiddleware = async (
 //     request: Request,
@@ -12,12 +16,26 @@
 //         const users = await userModel.find({});
 //         await Promise.all(
 //             users.map(async user => {
+//                 const soloStreaks = await soloStreakModel.find({ userId: user._id, status: StreakStatus.live });
+//                 const soloStreaksOrder = soloStreaks.map(soloStreak => String(soloStreak._id));
+//                 console.log('soloStreaks', soloStreaksOrder.length);
+//                 const challengeStreaks = await challengeStreakModel.find({
+//                     userId: user._id,
+//                     status: StreakStatus.live,
+//                 });
+//                 const challengeStreaksOrder = challengeStreaks.map(challengeStreak => String(challengeStreak._id));
+//                 console.log('challengeStreaks', challengeStreaksOrder.length);
+//                 const teamMemberStreaks = await teamMemberStreakModel.find({
+//                     userId: user._id,
+//                     active: true,
+//                 });
+//                 const teamStreaksOrder = teamMemberStreaks.map(teamMemberStreak => teamMemberStreak.teamStreakId);
+//                 console.log('teamStreaksOrder', teamStreaksOrder.length);
 //                 await userModel.findByIdAndUpdate(user._id, {
 //                     $set: {
-//                         onboarding: { whyDoYouWantToBuildNewHabitsChoice: null },
-//                         hasUsernameBeenCustomized: true,
-//                         hasCompletedOnboarding: true,
-//                         hasUsernameBeenCustomize: null,
+//                         soloStreaksOrder,
+//                         challengeStreaksOrder,
+//                         teamStreaksOrder,
 //                     },
 //                 });
 //             }),
