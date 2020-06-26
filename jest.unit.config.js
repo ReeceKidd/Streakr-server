182f267f4e183d57d2b8dd4856b26f7d204ce389
Reece Kidd <reecekidd95@gmail.com>
2020-06-26 17:44:37 +0100

    refactor: Dropped down threshold for unit test coverage

--- jest.unit.config.js
+++ jest.unit.config.js
@@ -2,10 +2,10 @@ module.exports = {
     preset: 'ts-jest',
     coverageThreshold: {
         global: {
-            branches: 90,
-            functions: 90,
-            lines: 90,
-            statements: 90,
+            branches: 85,
+            functions: 85,
+            lines: 85,
+            statements: 85,
         },
     },
     testEnvironment: 'node',
