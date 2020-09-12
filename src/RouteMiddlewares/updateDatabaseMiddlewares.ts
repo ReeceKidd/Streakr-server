7172088a7208ac729f97f54c75538d375438d5b2
Reece Kidd <reecekidd95@gmail.com>
2020-09-12 21:36:05 +0100

    chore: Updated sdk and shared

--- src/RouteMiddlewares/updateDatabaseMiddlewares.ts
+++ src/RouteMiddlewares/updateDatabaseMiddlewares.ts
@@ -1,17 +1,16 @@
 import { Request, Response } from 'express';
 
 import { teamMemberStreakModel } from '../Models/TeamMemberStreak';
-import { userModel } from '../Models/User';
+import { teamStreakModel } from '../Models/TeamStreak';
 
 export const updateDatabaseMiddleware = async (request: Request, response: Response): Promise<void> => {
     const teamMemberStreaks = await teamMemberStreakModel.find({});
     await Promise.all(
         teamMemberStreaks.map(async teamMemberStreak => {
-            const user = await userModel.findById(teamMemberStreak.userId);
+            const teamStreak = await teamStreakModel.findById(teamMemberStreak.teamStreakId);
             return teamMemberStreakModel.findByIdAndUpdate(teamMemberStreak._id, {
                 $set: {
-                    userProfileImage: user && user.profileImages && user.profileImages.originalImageUrl,
-                    username: user && user.username,
+                    status: teamStreak && teamStreak.status,
                 },
             });
         }),
