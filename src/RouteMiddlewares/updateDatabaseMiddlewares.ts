8384f2b4e0c33f2b1cec4f54b337172057724aea
Reece Kidd <reecekidd95@gmail.com>
2020-09-12 21:43:13 +0100

    chore: Script to update status for teamMemberStreaks

--- src/RouteMiddlewares/updateDatabaseMiddlewares.ts
+++ src/RouteMiddlewares/updateDatabaseMiddlewares.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express';
 
 import { teamMemberStreakModel } from '../Models/TeamMemberStreak';
 import { teamStreakModel } from '../Models/TeamStreak';
+import StreakStatus from '@streakoid/streakoid-models/lib/Types/StreakStatus';
 
 export const updateDatabaseMiddleware = async (request: Request, response: Response): Promise<void> => {
     const teamMemberStreaks = await teamMemberStreakModel.find({});
@@ -10,7 +11,7 @@ export const updateDatabaseMiddleware = async (request: Request, response: Respo
             const teamStreak = await teamStreakModel.findById(teamMemberStreak.teamStreakId);
             return teamMemberStreakModel.findByIdAndUpdate(teamMemberStreak._id, {
                 $set: {
-                    status: teamStreak && teamStreak.status,
+                    status: (teamStreak && teamStreak.status) || StreakStatus.archived,
                 },
             });
         }),
